076af6b51963fe3f10892557fc9fdb139499e039
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-09-08 13:04:02 +0500

    project initialized

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen text-3xl font-semibold">Unflitered Blogs</div>
+  );
+}
